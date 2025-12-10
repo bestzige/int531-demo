ab7ac1386df07b1c0a6abc7ce440b128b97bc62a
BestZige <bestzige@hotmail.com>
2025-12-10 23:39:11 +0700

    chore: add default metrics

--- src/lib/metrics.ts
+++ src/lib/metrics.ts
@@ -2,6 +2,8 @@ import client from 'prom-client';
 
 export const register = new client.Registry();
 
+client.collectDefaultMetrics({ register });
+
 export const httpRequestsTotal = new client.Counter({
   name: 'http_requests_total',
   help: 'Total HTTP requests',
--- src/lib/withMetrics.ts
+++ src/lib/withMetrics.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+
 import {
   activeRequests,
   httpRequestDurationSeconds,
@@ -13,23 +14,29 @@ export const withMetrics =
     activeRequests.inc();
     try {
       const res = await handler(req);
+
       const diff = process.hrtime(start);
       const duration = diff[0] + diff[1] / 1e9;
+
       httpRequestsTotal.inc({
         method: req.method,
         route: req.url || '/',
         status: res.status,
       });
+
       httpRequestDurationSeconds.observe(
         { method: req.method, route: req.url || '/', status: res.status },
         duration
       );
-      if (res.status >= 500)
+
+      if (res.status >= 500) {
         httpRequestsErrorsTotal.inc({
           method: req.method,
           route: req.url || '/',
           status: res.status,
         });
+      }
+
       return res;
     } finally {
       activeRequests.dec();
